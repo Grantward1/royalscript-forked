5d92faf0f1dd220e2276a64018cce5ebf7e53bb5
Grant Ward <gw1@email.com>
2021-11-09 16:28:15 -0700

    Separate royallib to another package
    - Remove royallib, now available in NPM package 'royallib'
    - Update license
    - Increment package number

--- test/core.test.js
+++ test/core.test.js
@@ -2,7 +2,6 @@
 
 const compiler = require('../src/compiler.js');
 const cmp = compiler.Compile;
-const royallib = require('../src/royallib.js');
 
 describe('Ensure correctness of core language features', () => {
   test('Perform simple arithmetic', () => {
@@ -17,15 +16,5 @@ describe('Ensure correctness of core language features', () => {
     const expr = (
       'defun(summinus1, args(x, y), -(+(x, y), 1)),summinus1(10, 9)');
     expect(eval(cmp(expr))).toBe(18);
-  });
-});
-
-describe('Ensure correctness of royallib functions', () => {
-  test('Test conditionals', () => {
-    expect(royallib.even(5)).toBe(false);
-    expect(royallib.even(4)).toBe(true);
-
-    expect(royallib.odd(5)).toBe(true);
-    expect(royallib.odd(4)).toBe(false);
   });
 });
